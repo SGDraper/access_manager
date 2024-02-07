a34afe598e441030ea5357061659fe3121a7610f
loopdom <giandomenicoloparco@gmail.com>
2024-02-07 15:41:28 +0000

    2nd change

--- access_manager/lib/age_checker.py
+++ access_manager/lib/age_checker.py
@@ -1,4 +1,10 @@
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
+# from dateutil.relativedelta import relativedelta
 
 def age_checker(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise Exception("Incorrect format, format required YYYY-mm-dd")
+    
+    
--- access_manager/tests/test_age_checker.py
+++ access_manager/tests/test_age_checker.py
@@ -1,6 +1,15 @@
 from lib.age_checker import *
 import pytest
 
-def tests_for_string():
-    age_checker(string)
-    assert type(string) == str
+def test_date_format():
+
+    with pytest.raises(Exception) as e:
+        age_checker("string")
+    message = str(e.value)
+    
+    assert message == "Incorrect format, format required YYYY-mm-dd"
+
+def test_user_under_16():
+    result = age_checker("2019-05-01")
+
+    assert result == "Access denied. Minimum age required is 16. Your age is 4"
